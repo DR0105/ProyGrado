100c8a6f7c8acf8373686e6556b293c703b645fd
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-03-19 23:06:13 -0500

    Se Agrega plantilla inicial para historia clínca

--- src/app/pages/citas/historia-clinica/historia-clinica.component.ts
+++ src/app/pages/citas/historia-clinica/historia-clinica.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class HistoriaClinicaComponent implements OnInit {
-
+  panelOpenState = false;
   constructor() { }
 
   ngOnInit() {
