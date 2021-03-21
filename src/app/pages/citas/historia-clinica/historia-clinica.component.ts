2541bbe023eb5a295bd2f5e566edf0af30ded359
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-03-21 15:04:24 -0500

    Se pasa a formularios reactivos los datos básicos

--- src/app/pages/citas/historia-clinica/historia-clinica.component.ts
+++ src/app/pages/citas/historia-clinica/historia-clinica.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'ngx-historia-clinica',
@@ -7,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HistoriaClinicaComponent implements OnInit {
   panelOpenState = false;
-  constructor() { }
-  persona: any = [
-    { nombre: 'Benito Suarez', codigo: 20212021021, docuento: 2021202120, eps: 'Famisanar', fechaDeNacimiento: 20 - 21 - 2021, vinculacion: { docente: true }, otro: 'xxxxxx' }]
+  constructor(private fb: FormBuilder) { }
+
+  datosBasicos: FormGroup = this.fb.group({
+    vinculacion: ['', Validators.required],
+    tipo: ['', Validators.required],
+    situacionAcademica: ['', Validators.required],
+  })
+  historiaMedicina: FormGroup = this.fb.group({
+    motivoConsulta: [''],
+  })
+  prueba = {
+    nombre: 'Benito Suárez', codigo: 20212021021, documento: 2021202120, eps: 'Famisanar', fechaDeNacimiento: new Date(),
+    lugarDeNacimiento: 'Bogotá', edad: 20, vinculacion: 'Docente', tipo: 'Otro', cualTipo: 'xxxxxx', dependencia: null, procedenteDe: null,
+    facultad: 'Ingeniería', carrera: 'Ingeniería de Sistemas', direccion: 'Calle falsa 123', telefono: '2021202', contactoEmergencia: 'María Camelo',
+    parentesco:'Madre',direccionContacto: 'Calle falsa 123', telefonoContacto: '2021202', situacionAcademica:'Otro', cualSituacion:'xxxxxx'
+  }
   ngOnInit() {
   }
-
 }
