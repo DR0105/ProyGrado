1051529f3ecc3b882ef03e7471f82982fd0993f8
Bryan Luis <baluist@correo.udistrital.edu.co>
2021-03-22 13:38:09 -0500

    Agregado análisis y plan de manejo

--- src/app/pages/citas/historia-clinica/historia-clinica.component.ts
+++ src/app/pages/citas/historia-clinica/historia-clinica.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'ngx-historia-clinica',
@@ -15,6 +16,7 @@ export class HistoriaClinicaComponent implements OnInit {
     tipo: ['', Validators.required],
     situacionAcademica: ['', Validators.required],
   })
+  nuevoAnalisis: FormControl = this.fb.control('', Validators.required);
   medicinaForm: FormGroup = this.fb.group({
     motivoConsulta: ['', Validators.required],
     patologicos: ['', Validators.required],
@@ -50,11 +52,33 @@ export class HistoriaClinicaComponent implements OnInit {
     cardioVascular: ['', Validators.required],
     neurologico: ['', Validators.required],
     respiratorio: ['', Validators.required],
-    examenes:['',Validators.required]
-
-
-
+    examenes: ['', Validators.required],
+    ta: ['', Validators.required],
+    fc: ['', Validators.required],
+    fr: ['', Validators.required],
+    sao2: ['', Validators.required],
+    peso: ['', Validators.required],
+    imc: ['', Validators.required],
+    talla: ['', Validators.required],
+    tc: ['', Validators.required],
+    estadoGeneral: ['', Validators.required],
+    cabezaYCuello: ['', Validators.required],
+    orl: ['', Validators.required],
+    ojos: ['', Validators.required],
+    torax: ['', Validators.required],
+    ruidosRespiratorios: ['', Validators.required],
+    ruidosCardiacos: ['', Validators.required],
+    abdomen: ['', Validators.required],
+    neurologicoE: ['', Validators.required],
+    genital: ['', Validators.required],
+    extremidades: ['', Validators.required],
+    id1: ['', Validators.required],
+    id2: ['', Validators.required],
+    id3: ['', Validators.required],
+    id4: ['', Validators.required],
+    analisis: this.fb.array([])
   })
+  model: NgbDateStruct;
   prueba = {
     nombre: 'Benito Suárez', codigo: 20212021021, documento: 2021202120, eps: 'Famisanar', fechaDeNacimiento: new Date(),
     lugarDeNacimiento: 'Bogotá', edad: 20, vinculacion: 'Docente', tipo: 'Otro', cualTipo: 'xxxxxx', dependencia: null, procedenteDe: null,
@@ -62,5 +86,18 @@ export class HistoriaClinicaComponent implements OnInit {
     parentesco: 'Madre', direccionContacto: 'Calle falsa 123', telefonoContacto: '2021202', situacionAcademica: 'Otro', cualSituacion: 'xxxxxx'
   }
   ngOnInit() {
+  }
+  get analisisArr() {
+    return this.medicinaForm.get('analisis') as FormArray;
+  }
+  agregarAnalisis() {
+    if (this.nuevoAnalisis.invalid) {
+      return
+    }
+    this.analisisArr.push(new FormControl(this.nuevoAnalisis.value, Validators.required));
+    this.nuevoAnalisis.reset();
+  }
+  borrarAnalisis(i: number) {
+    this.analisisArr.removeAt(i);
   }
 }
